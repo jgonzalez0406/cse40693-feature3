56ea5f214fcf18c1e0c602ffe0dd3bfb22c745ea
Juliana Gonzalez <134644224+jgonzalez0406@users.noreply.github.com>
2025-02-20 16:34:35 -0500

    Update App.js

--- App.js
+++ App.js
@@ -4,20 +4,14 @@ import {
 } from "https://unpkg.com/htm/preact/standalone.module.js";
 import { AddExpenseForm } from "./Components/ExpenseForm/AddExpenseForm.js"; // Importing the child components
 import Navbar from "./Components/Navigation/NavBar.js";
+import expenseForm from "./Components/ExpenseForm/ExpenseForm.js";
 
 // App Component
 function App() {
-  function handleAddExpense() {
-    alert("Expense submitted");
-  }
-
   return html`
     <div>
       <${Navbar} />
-      <h1>Add a New Expense</h1>
-      <p>Please fill out the details below.</p>
-      <hr />
-      <${AddExpenseForm} onAddExpense=${handleAddExpense} />
+      <${expenseForm()}< />
     </div>
   `;
 }
